7db2574bf69f2a117fc87f12fb144ba5d31fe703
parksb <parkgds@gmail.com>
2020-12-28 11:25:21 +0900

    fix: 다양한 상태 코드를 처리하도록 변경

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
 STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$1" -v)
-if [[ $STATUS_CODE -ne 200 ]]; then
-  echo status code: $STATUS_CODE
+VALID_STATUS_CODES=(200 301)
+
+echo status code: $STATUS_CODE
+if [[ ${VALID_STATUS_CODES[@]} =~ "${STATUS_CODE}" ]]; then
+  exit 0
+else
   exit -1
 fi
+
