0331df6f0a390c505ecbb8725e0887e7ac7dc42d
parksb <parkgds@gmail.com>
2021-02-16 22:53:47 +0900

    fix: curl 명령에 insecure option 추가

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$1" -v)
+STATUS_CODE=$(curl -k -s -o /dev/null -w "%{http_code}" "$1" -v)
 VALID_STATUS_CODES=(200 301)
 
 echo status code: $STATUS_CODE
